59844ab16c9e69d6f906ffa07a0d06c43e540d52
Lacwil <lachlan@yoursoundtrak.com>
2025-05-04 19:57:30 +1000

    Force global text wrap and overflow fix from root level

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div
+      style={{
+        width: '100%',
+        maxWidth: '100%',
+        overflowX: 'hidden',
+        whiteSpace: 'normal',
+        wordWrap: 'break-word',
+        overflowWrap: 'break-word',
+        display: 'block',
+        fontFamily: 'system-ui, sans-serif',
+      }}
+    >
+      <App />
+    </div>
+  </React.StrictMode>
+);
